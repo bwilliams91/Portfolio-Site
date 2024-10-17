3b8b168b7697b335f1c357233b45dd26c9b64157
Brian Williams <brianw.developer@gmail.com>
2024-10-16 18:41:49 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -75,6 +75,7 @@ const NavBar = () => {
         <CustomLink href="/about" title="About" className="mx-4" />
         <CustomLink href="/projects" title="Projects" className="mx-4" />
         <CustomLink href="/articles" title="Articles" className="ml-4" />
+        <CustomLink href="/fun" title="Fun Things" className="ml-4" />
       </nav>
       
       <nav className="flex items-center justify-center flex-wrap gap-6">
