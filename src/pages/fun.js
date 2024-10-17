f3d50dbd3fe0a285e47e4937d9fafb311008d64a
Brian Williams <brianw.developer@gmail.com>
2024-10-16 18:41:51 -0600

    Create fun.js

---
+++ src/pages/fun.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import TransitionEffect from "@/components/TransitionEffect";
+import Layout from "@/components/layout";
+import DndCharacterGenerator from "@/components/D&DCharacterGenerator"
+
+const fun = () => {
+    return (
+        <>
+            <Head>
+                <title>Brian Williams | Fun Things</title>
+                <meta name="Fun Things" content="A page of little widgets to play with using various techniques in modern development" />
+            </Head>
+            <TransitionEffect />
+            <main className="w-full pb-16 mb-16 flex flex-col items-center justify-center overflow-hidden">
+                <Layout className="pt-16 ">
+                    <h1 className="text-light">Hello world</h1>
+                    <DndCharacterGenerator />
+                </Layout>
+            </main>
+        </>
+    );
+}
+
+export default fun;
