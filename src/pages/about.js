2d33eb6ce9f531c444eb06cf6ca3fa3ed0f796bd
Brian Williams <brianw.developer@gmail.com>
2025-01-24 13:41:09 -0700

    Update about.js
    adjusted years of experience

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,7 +54,7 @@ const about = () => {
               <p className="font-medium">
                 Hello! My name is Brian, I am a web developer and UI/UX designer
                 with a passion for creating beautiful, functional, and
-                user-centered digital experiences. With 2 years of experience in
+                user-centered digital experiences. With 4 years of experience in
                 the field. I am always looking for new and innovative ways to
                 bring my clients visions to life.
               </p>
@@ -103,7 +103,7 @@ const about = () => {
 
               <div className="flex flex-col items-end justify-center xl:items-center">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                  <AnimatedNumbers value={2} />+
+                  <AnimatedNumbers value={4} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Years of Experience
